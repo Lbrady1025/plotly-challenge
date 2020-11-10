b04b91948bb04935a2a1a07a96f98c94206f105d
Leah Brady <brady.leah@gmail.com>
2020-11-09 20:44:56 -0500

    start pulling info from JSON

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,12 @@
+function readJSON(id) {
+    d3.json("../samples.json").then( function(data) {
+        console.log(data);
+    });
+    // var ids = data.samples[0].otu_ids;
+    // console.log(ids);
+    // var sample_ids = data.samples[0].sample_ids;
+    // console.log(sample_ids);
+    // var labels = data.samples[0].otu_labels;
+    // console.log(labels);
 
-d3.json("../samples.json").then( function(data) {
-    console.log(data);
-});
-
+};
