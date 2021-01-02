96a5300371cbcf7f8be88d78bfea25ebb44caf88
Leah Brady <brady.leah@gmail.com>
2021-01-02 09:45:35 -0500

    clean up readJSON functions and add test subjects to dropdown

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,24 +1,26 @@
-function readJSON(id) {
-    d3.json("../samples.json").then( function(data) {
-        console.log(data);
+function readJSON(sample) {
+    d3.json("../../data/samples.json").then( function(data) {
+        //console.log(data);
+    
+    var ids = data.samples[0].otu_ids;
+    console.log(ids);
+    var sample_ids = data.samples[0].sample_ids;
+    console.log(sample_ids);
+    var labels = data.samples[0].otu_labels;
+    console.log(labels);
     });
-    // var ids = data.samples[0].otu_ids;
-    // console.log(ids);
-    // var sample_ids = data.samples[0].sample_ids;
-    // console.log(sample_ids);
-    // var labels = data.samples[0].otu_labels;
-    // console.log(labels);
-
 };
 
 function init() {
     var dropdown = d3.select("#selDataset");
 
-    d3.json("../samples.json").then( function(data) {
-        console.log(data);
+    d3.json("../../data/samples.json").then( function(data) {
+        //console.log(data);
 
         data.names.forEach( function (name) {
             dropdown.append("option").text(name).property("value");
+
+        readJSON();
     });
     });
 
