22f638b20ec1341215cbe0737d009cfded8c805b
Leah Brady <brady.leah@gmail.com>
2021-01-02 10:48:06 -0500

    fix issues in readjSON and addCharts functions, add bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,23 +10,47 @@ function readJSON(sample) {
 
     panel.html("");
 
-    Object.entires(result).foreach(([key,value]) => panel.append("h6").text(`${key.toUpperCase()}: ${value}`))
+    Object.entries(result).forEach(([key,value]) => panel.append("h6").text(`${key.toUpperCase()}: ${value}`))
 
     });
 };
 
 function addCharts (sample) {
     d3.json("../../data/samples.json").then( function(data) {
-        var sample = data.sample;
-        var getResult = sample.filter(sampleArr => sample.id == sample);
+        var samples = data.samples;
+        var getResult = samples.filter(sampleArr => sampleArr.id == sample);
         var result = getResult[0];
 
-        var ids = result.otu_ids;
-        console.log(ids);
-        var sample_ids = result.sample_ids;
-        console.log(sample_ids);
-        var labels = result.otu_labels;
-        console.log(labels);
+        var otu_ids = result.otu_ids;
+        // console.log(ids);
+        var sample_values = result.sample_values;
+        // console.log(sample_ids);
+        var otu_labels = result.otu_labels;
+        // console.log(labels);
+
+        var bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: { t: 0 },
+            hovermode: "closest",
+            xaxis: { title: "OTU ID" },
+            margin: { t: 30}
+          };
+          var bubbleData = [
+            {
+              x: otu_ids,
+              y: sample_values,
+              text: otu_labels,
+              mode: "markers",
+              marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+              }
+            }
+          ];
+      
+          Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+      
     })
     
 
@@ -38,9 +62,14 @@ function init() {
     d3.json("../../data/samples.json").then( function(data) {
         //console.log(data);
 
-        data.names.forEach( function (name) {
+        names = data.names;
+
+        names.forEach( function (name) {
             dropdown.append("option").text(name).property("value");
 
+        var initSample = names[0];
+        readJSON(initSample);
+        addCharts(initSample);
     });
     });
 
