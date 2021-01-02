5fb0acae6872576d150cd5712c0b5949babf86c6
Leah Brady <brady.leah@gmail.com>
2021-01-02 10:14:58 -0500

    add metadata build

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,14 +1,27 @@
 function readJSON(sample) {
     d3.json("../../data/samples.json").then( function(data) {
         //console.log(data);
-    
+    var metadata = data.metadata;
+
+    var getResult = metadata.filter(sampleArr => sampleArr.id == sample);
+    var result = getResult[0];
+
+    var panel = d3.select("#sample-metadata");
+
+    panel.html("");
+
+    Object.entires(result).foreach(([key,value]) => panel.append("h6").text(`${key.toUpperCase()}: ${value}`))
+
+    });
+};
+
+function addCharts () {
     var ids = data.samples[0].otu_ids;
     console.log(ids);
     var sample_ids = data.samples[0].sample_ids;
     console.log(sample_ids);
     var labels = data.samples[0].otu_labels;
     console.log(labels);
-    });
 };
 
 function init() {
@@ -20,7 +33,6 @@ function init() {
         data.names.forEach( function (name) {
             dropdown.append("option").text(name).property("value");
 
-        readJSON();
     });
     });
 
